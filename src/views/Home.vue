e6024b9bd6386acebdc9b1ff854eaf26cf3ee48a
Adetutu <adetutugbangbola@gmail.com>
2021-07-24 21:59:58 +0100

    update on styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,23 @@
      <section>
     <div class="container">
       <div class="row">
-        <div class="col-md-6 fir-col" id="pix-first">
+        <div class="col-md-6 fir-col animate__animated animate__fadeInLeft" id="pix-first">
           <h1 class="purchase">Each Purchase will be made with pleasure</h1>
           <p class="global pt-3">We work with global brands and have  created <br> an application for you to get started</p>
           <p class="global pt-3">We work with global brands and have  created <br> an application for you to get started</p>
+
+          <div class="class-btn mt-3 pt-4">
+     <router-link :to="{name: 'Catalog'}" class="shop-btn">Shop Now</router-link>
+    
+    </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 animate__animated animate__fadeInRight">
         
           <img src="../../src/assets/Img/shopping-pix.png" class="" style="width: 75%;" alt="">
         </div>
       </div>
       
-      <div class="class-btn mt-3">
-     <router-link :to="{name: 'Catalog'}" class="shop-btn">Shop Now</router-link>
-    
-    </div>
+      
     </div>
     
   <Catalog/>
@@ -55,6 +57,17 @@
 
 .class-btn{
     margin-bottom: 5rem;
+}
+
+.shop-btn:hover{
+ border: 2px solid rgb(226, 27, 226);
+color: rgb(27, 27, 25);
+background: inherit;
+
+}
+
+.animate__animated{
+animation-duration: 1.7s ;
 }
 
 @media screen and (max-width: 480px) {
